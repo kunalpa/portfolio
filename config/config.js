9531f4cf6bc97ea83eb408cab5da89cb684cac52
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-05-31 15:36:44 -0400

    Inputed my data

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-	name: "Hashir",
+	name: "Kunal",
 	links: [
 		{
 			title: "About",
@@ -25,8 +25,8 @@ export const navigation = {
 	],
 }
 export const intro = {
-	title: "Hey, I'm Hashir",
-	description: "A Computer Engineer creating mobile apps and static websites.",
+	title: "Hey, I'm Kunal",
+	description: "A Computer Science and Computational Physics student interested in computer vision.",
 	image: profile.src,
 	buttons: [
 		{
@@ -36,7 +36,7 @@ export const intro = {
 		},
 		{
 			title: "Resume",
-			link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+			link: "https://docs.google.com/document/d/1mr0YNYw-Ab3Ijo0LJTuXfBBO7ldJdQlN8zc4dLAViqA/edit?usp=sharing",
 			isPrimary: false,
 		},
 	],
@@ -45,9 +45,9 @@ export const intro = {
 export const about = {
 	title: "Who I am",
 	description: [
-		"I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-		"The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-		"When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+		"I am a rising Junior studying Computer Science and Computational Physics at UNC-Chapel Hill.",
+		"I am currently working for Fidelity Investments as a Full Stack Intern. I have taken special interest in computer vision and hope to complete projects utilizing pretrained tensorflow models for object detection.",
+		"When I’m not programming, I enjoy weightlifting, gaming, and playing basketball with friends.",
 	],
 }
 
@@ -55,13 +55,13 @@ export const work = {
 	title: "What I do",
 	cards: [
 		{
-			title: "Mobile App Development",
-			description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+			title: "Backend Development",
+			description: "I work on backend projects ranging from tracking the stock market to automating deployment using Kubernetes.",
 			icons: null,
 		},
 		{
-			title: "Web Development",
-			description: "I create responsive static websites using Reactjs.",
+			title: "Numerical Methods",
+			description: "I use differential equations to model immerging physics and mathematics problems.",
 			icons: null,
 		}
 	],
@@ -71,40 +71,32 @@ export const projects = {
 	title: "Projects",
 	cards: [
 		{
-			title: "StarBook",
-			description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+			title: "Covid-19 Effects Dashboard",
+			description: "A dashboard built to track the effects of Covid-19 at a global scale.",
 			icons: [
-				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-				},
 				{
 					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/star_book",
-				},
-			]
-		},
-		{
-			title: "QuranTalk",
-			description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
-			icons: [
-				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-				},
-				{
-					icon: faGooglePlay,
-					link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
+					link: "https://github.com/comp426-2022-spring/a99-atlas",
 				},
 			]
 		},
 		{
 			title: "Portfolio",
-			description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
+			description: "A collection of projects I completed before understanding how Git and GitHub worked",
 			icons: [
 				{
 					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/portfolio",
+					link: "https://github.com/kunalpa/Portfolio",
+				},
+			]
+		},
+		{
+			title: "___FIX___",
+			description: "",
+			icons: [
+				{
+					icon: faGithub,
+					link: "",
 				},
 			]
 		},
@@ -113,16 +105,16 @@ export const projects = {
 
 export const contact = {
 	title: "Get in touch",
-	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+	description: "If you have any questions, comments, or just want to say hi, feel free to email or call me.",
 	buttons: [
 		{
 			title: "Email Me",
-			link: "mailto:hashirshoaeb@gmail.com",
+			link: "mailto:kunal.pathak@unc.edu",
 			isPrimary: true,
 		},
 		{
-			title: "Schedule Meeting",
-			link: "",
+			title: "Call Me",
+			link: "(919)448-1227",
 			isPrimary: false,
 		},
 	]
@@ -131,35 +123,27 @@ export const contact = {
 // SEARCH ENGINE 
 export const SEO = {
 	// 50 - 60 char  
-	title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-	description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+	title: "Kunal Pathak | Computer Scientist | Python and Java developer",
+	description: "I am interested in software engineering.",
 	image: profile.src,
 }
 
 export const links = {
 	image: profile.src,
-	title: "@hashirshoaeb",
-	description: "Computer Engineer | Flutter | Reactjs Developer",
+	title: "@kunalpa",
+	description: "Software Engineer | Computer Scientist | Python and Java Developer",
 	cards: [
 		{
 			title: "My website",
-			link: "https://hashirshoaeb.com/",
-		},
-		{
-			title: "QuranTalk App",
-			link: "https://www.qurantalk.app/",
-		},
-		{
-			title: "StarBook App",
-			link: "https://starbook.dev/",
+			link: "",
 		},
 		{
 			title: "My GitHub",
-			link: "https://github.com/hashirshoaeb/",
+			link: "https://github.com/kunalpa/",
 		},
 		{
 			title: "My LinkedIn",
-			link: "https://www.linkedin.com/in/hashirshoaeb/",
+			link: "https://www.linkedin.com/in/kunalhpathak/",
 		},
 	]
 }
